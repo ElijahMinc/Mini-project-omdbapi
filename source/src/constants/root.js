f1ad9dd6cb4fafd714e369d5ab155a80ebb4c608
ElijahMinc <gvidar89@gmail.com>
2021-08-06 16:32:12 +0300

    Remove directory

--- source/src/constants/root.js
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_LIMIT = 12
-export const API_KEY = "a4b1e3df";
-export const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
-
--- source/src/store/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import reposReducer from "./reducers/reposReducer";
-
-export default combineReducers({
-   reposReducer
-});
